7c75a53557df477968198df36cd79b9cf8237e04
Michael Ilseman <michael.ilseman@gmail.com>
2010-05-30 22:19:38 -0500

    Added functionality, and utilities for the controller

--- Dnd/DM/CombatTracker/Core.hs
+++ Dnd/DM/CombatTracker/Core.hs
@@ -8,13 +8,14 @@ module Dnd.DM.CombatTracker.Core where
   import qualified Text.PrettyPrint.HughesPJ as P
   import Text.PrettyPrint.HughesPJ (($+$), (<>), ($$), text, nest)
 
-  type Agent = String
+  type FieldName = String
+  type EntryName = String
 
   -- I thought about doing this with existential types, but didn't want rank-2 types
   -- So I decided to just make this a synonym for Int
   type Info = Int
 
-  type Entry = (Agent, AssocList String Info)
+  type Entry = (EntryName, AssocList FieldName Info)
   type Character = Entry
   type Effect = Entry
   type Marker = Entry
@@ -36,45 +37,63 @@ module Dnd.DM.CombatTracker.Core where
   roundNumber w = unsafeFetch "Round-Number" (unsafeFetch "Top-Of-Round" w)
 
   -- | Create a character given name and initiative
-  createCharacter :: String -> Int -> Character
+  createCharacter :: EntryName -> Int -> Character
   createCharacter s i = (s, [("Initiative", i)])
 
   -- | Add a character to a combat wheel
-  addCharacter :: String -> Int -> CombatWheel -> CombatWheel
+  addCharacter :: EntryName -> Int -> CombatWheel -> CombatWheel
   addCharacter s i = insertAt (isBefore char) char Before
     where char = createCharacter s i
 
   -- | List version of addCharacter
-  addCharacters :: [(String,Int)] -> CombatWheel -> CombatWheel
+  addCharacters :: [(EntryName,Int)] -> CombatWheel -> CombatWheel
   addCharacters cs w = foldl (flip (uncurry addCharacter)) w cs
 
   -- | Create a monster, given name, initiative, and hp
-  createMonster :: String -> Int -> Int -> Monster
+  createMonster :: EntryName -> Int -> Int -> Monster
   createMonster s i hp = (s, add entry "HP" ( hp))
     where entry = snd $ createCharacter s i
 
   -- | Add a monster to a combat wheel
-  addMonster :: String -> Int -> Int -> CombatWheel -> CombatWheel
+  addMonster :: EntryName -> Int -> Int -> CombatWheel -> CombatWheel
   addMonster s i hp = insertAt (isBefore mon) mon Before
     where mon = createMonster s i hp
 
   -- | Create an effect given name and duration
-  createEffect :: String -> Int -> Effect
+  createEffect :: EntryName -> Int -> Effect
   createEffect s i = (s, [("Duration", i)])
 
   -- | Add an effect, such as an ongoing spell, and insert it at the given position
   -- relative to the specified entry
-  addEffect :: String -> Int -> Position -> String -> CombatWheel -> CombatWheel
+  addEffect :: EntryName -> Int -> Position -> EntryName -> CombatWheel -> CombatWheel
   addEffect s i pos entry = insertAt (isEntry entry) (createEffect s i) pos
 
 
   -- | Remove the Entry whose name is passed in
-  remove :: String -> CombatWheel -> CombatWheel
+  remove :: EntryName -> CombatWheel -> CombatWheel
   remove = flip del
+
+  -- | Move the Entry named to pos relative to third argument
+  -- Note: undefined when the entry name is not present
+  move :: EntryName -> Position -> EntryName -> CombatWheel -> CombatWheel
+  move tomove pos target w = insertAt (isEntry target) entry pos (del w tomove)
+    where entry = (tomove, unsafeFetch tomove w)
+
+  -- | Update a field in the named Entry to a new value, adding it if it's missing
+  updateField :: EntryName -> FieldName -> Info -> CombatWheel -> CombatWheel
+  updateField s fieldname i (e:es) = if isEntry s e
+                                     then (s, add (snd e) fieldname i) : es
+                                     else e : updateField s fieldname i es
+
+  -- | Remove a field in the named Entry 
+  removeField :: EntryName -> FieldName -> CombatWheel -> CombatWheel
+  removeField s fieldname (e:es) = if isEntry s e
+                                   then (s, del (snd e) fieldname) : es
+                                   else e : removeField s fieldname es
 
 
   -- | Damage a monster
-  damage :: String -> Int -> CombatWheel -> CombatWheel
+  damage :: EntryName -> Int -> CombatWheel -> CombatWheel
   damage s i w = replace (createMonster s initiative newhp) (isEntry s) w
     where initiative = unsafeFetch "Initiative" monster
           newhp = unsafeFetch "HP" monster - i
@@ -82,7 +101,7 @@ module Dnd.DM.CombatTracker.Core where
 
   -- | Heal a monster
   -- Does NOT know or follow any game mechanics, such as max hp or tmp hp
-  heal :: String -> Int -> CombatWheel -> CombatWheel
+  heal :: EntryName -> Int -> CombatWheel -> CombatWheel
   heal s i = damage s (0-i)
 
 
@@ -90,15 +109,15 @@ module Dnd.DM.CombatTracker.Core where
   -- Will delay until the end of the wheel if given character's name does not
   -- exist in combat wheel (i.e. a single rotation without update)
   -- Note: Doesn't actually update the initiative value
-  delay :: String -> CombatWheel -> CombatWheel
-  delay s = move (isEntry s) After
+  delay :: EntryName -> CombatWheel -> CombatWheel
+  delay s = moveTo (isEntry s) After
 
 
   -- | Advance the round, processing durations, round numbers, hp etc
   -- Does NOT know or follow game mechanics, such as bleeding out
   advance :: CombatWheel -> CombatWheel
-  advance (e:es) | or [isOver e, isDead e] = es
-  advance (e:es) | isUnconscious e = es ++ [e]  -- skip its turn
+  advance (e:es) | or [isOver e, isStatus Dead e] = es
+  advance (e:es) | isStatus Unconscious e = es ++ [e]  -- skip its turn
   advance w = process updateEntry w
 
 
@@ -114,13 +133,20 @@ module Dnd.DM.CombatTracker.Core where
   -- | Doc for Entries
   entryDoc :: Entry -> Int -> P.Doc
   entryDoc (s, al) nesting = entryname <> text (":") $$ nest nesting (alDoc al)
-    where entryname = text $ if isUnconscious (s, al) then "<" ++ s ++ ">" else s
+    where entryname = text $ left ++ s ++ right
+          (left,right) = case getStatus (s, al) of
+                           Conscious   -> ("","")
+                           Staggered   -> ("*","*")
+                           Unconscious -> ("<", ">")
+                           Dead        -> ("[", "]")
+                           UNDEFINED   -> ("","")
 
   -- | Doc for Association Lists
   alDoc :: (Show b) => AssocList String b -> P.Doc
   alDoc ((a,b):al) = P.vcat (text "[ " <> innard a b : rest al) <> text " ]"
     where innard a b = text $ a ++ " => " ++ show b
           rest al = map (\(a,b) -> text ", " <> innard a b) al
+  alDoc [] = text "[]"
 
   -- | Pretty Print a combat wheel
   pp :: CombatWheel -> IO()
@@ -145,7 +171,7 @@ module Dnd.DM.CombatTracker.Core where
       (Just ( i), _) -> False
 
   -- | Predicate to match an entry with it's name
-  isEntry :: String -> Entry -> Bool
+  isEntry :: EntryName -> Entry -> Bool
   isEntry s c = fst c == s
 
   -- | Update the entry, i.e. decrement durations, increase turn counts, etc.
@@ -168,22 +194,21 @@ module Dnd.DM.CombatTracker.Core where
                      Just i -> i <= 0
                      _ -> False
 
-  -- | Predicate saying whether the current monster is unconscious
-  -- If HP is missing or > 0 then False, else True
-  isUnconscious :: Entry -> Bool
-  isUnconscious (a, al) = case lookup "HP" al of
-                            Just i -> i < 0
-                            Nothing -> False
+  data Status = Conscious | Unconscious | Dead | Staggered | UNDEFINED
+              deriving (Eq, Show)
 
-  -- | Predicate saying whether the current monster is dead
-  -- If HP is missing or > -10 then False, else True
-  isDead :: Entry -> Bool
-  isDead (a, al) = case lookup "HP" al of
-                            Just i -> i <= -10
-                            Nothing -> False
+  -- | Predicate saying whether the passed monster is status or not
+  isStatus :: Status -> Entry -> Bool
+  isStatus status e = status == getStatus e
 
-
-  -- Posible todos: handle monster's being unconscious, and bleeding out
+  getStatus :: Entry -> Status
+  getStatus (a, al) = case lookup "HP" al of
+                        Just i -> status i
+                        Nothing -> UNDEFINED
+    where status i | i > 0                = Conscious
+          status i | i == 0               = Staggered
+          status i | and [i > -10, i < 0] = Unconscious
+          status i | i <= -10             = Dead
 
 
   -- More to come
@@ -200,6 +225,8 @@ module Dnd.DM.CombatTracker.Core where
   combat6 = damage "Spider 1" 15 combat5
   combat7 = advance $ advance combat6
   combat8 = heal "Wolf 1" 2 combat7
+  combat9 = advance $ damage "Wolf 1" 9 combat8
+  combat10 = damage "Wolf 1" 2 combat9
 
   example1 = combatDoc combat1
   example2 = combatDoc combat2
@@ -209,3 +236,5 @@ module Dnd.DM.CombatTracker.Core where
   example6 = combatDoc combat6
   example7 = combatDoc combat7
   example8 = combatDoc combat8
+  example9 = combatDoc combat9
+  example10 = combatDoc combat10
