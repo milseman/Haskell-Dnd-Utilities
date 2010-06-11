4832dffe4b69e365e8d62e7fffd03f0585b2e283
Michael Ilseman <michael.ilseman@gmail.com>
2010-06-10 22:39:46 -0500

    Perrin and Crit explicitly spelled out

--- Dnd/Dice/DiceController.hs
+++ Dnd/Dice/DiceController.hs
@@ -1,6 +1,5 @@
 module Dnd.Dice.DiceController where
   import Dnd.Dice.DiceRoller (d, dc, Roll, RollHistory, ppRollHistory, perrinCrit)
---  import Text.ParserCombinators.Parsec
   import Control.Monad.State.Lazy
 
   data Expr = Value Int
@@ -8,8 +7,8 @@ module Dnd.Dice.DiceController where
             | Sub Expr Expr
             | Mult Expr Expr
             | D Expr Expr
-            | Dc Expr Expr
-            | P
+            | Crit Expr Expr
+            | Perrin
               deriving (Show, Eq, Read)
 
   type DiceResult = StateT [RollHistory] IO Int
@@ -20,23 +19,23 @@ module Dnd.Dice.DiceController where
   eval (Sub e1 e2) = performOnDiceResult (-) e1 e2
   eval (Mult e1 e2) = performOnDiceResult (*) e1 e2
   eval (D e1 e2) = rollToDiceResult (d) e1 e2
-  eval (Dc e1 e2) = rollToDiceResult (dc) e1 e2
-  eval P = perrinToDiceResult
+  eval (Crit e1 e2) = rollToDiceResult (dc) e1 e2
+  eval Perrin = perrinToDiceResult
 
-  -- Perform the provided binary operation on two expressions. Concatinates their HistoryLists
+  -- | Perform the provided binary operation on two expressions. Concatinates their HistoryLists
   performOnDiceResult :: (Int -> Int -> Int) -> Expr -> Expr -> DiceResult
   performOnDiceResult op e1 e2 = StateT $ \h -> do (leftV, leftH) <- runStateT (eval e1) []
                                                    (rightV, rightH) <- runStateT (eval e2) []
                                                    return (leftV `op` rightV, leftH ++ rightH)
 
-  -- Roll the provided roller on two expressions. Gets the new history and sticks it in
+  -- | Roll the provided roller on two expressions. Gets the new history and sticks it in
   rollToDiceResult :: (Int -> Int -> Roll) -> Expr -> Expr -> DiceResult
   rollToDiceResult op e1 e2 = StateT $ \h -> do (leftV, leftH) <- runStateT (eval e1) []
                                                 (rightV, rightH) <- runStateT (eval e2) []
                                                 (midV, midH) <- runStateT (leftV `op` rightV) []
                                                 return (midV, leftH ++ (midH : rightH))
 
-  -- Roll some Perrin rolls and give a result
+  -- | Roll some Perrin rolls and give a result
   perrinToDiceResult :: DiceResult
   perrinToDiceResult = StateT $ \h -> do (v, newH) <- runStateT perrinCrit []
                                          return (v, [newH])
