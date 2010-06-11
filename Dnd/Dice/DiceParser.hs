cdaf83c974303d6b6cd7f26100248eaedb351232
Michael Ilseman <michael.ilseman@gmail.com>
2010-06-10 22:38:47 -0500

    Overhauled parser: now uses a specialized Either monad to handle errors.

--- Dnd/Dice/DiceParser.hs
+++ Dnd/Dice/DiceParser.hs
@@ -7,36 +7,57 @@ module Dnd.Dice.DiceParser where
   -- After trying parsec for a while, I'm just going to handroll a custom parser
   -- This is not code that I'm particularly happy with. I'll come back around to fixing this later
 
+  -- We might turn a result, or we might turn an error
+  data ParseEither a = Result a | Error String
+                     deriving (Show, Eq, Read)
+
+  -- Propagate errors, otherwise keep the results flowing
+  instance Monad ParseEither where
+                           return = Result
+                           Error s >>= _ = Error s
+                           Result a >>= f = f a
+                           
+  type Tokens = [String]
+
+  type LexResult = ParseEither Tokens
+
+  type ParseResult = ParseEither Expr
+
+  -- | Bind 2 arguments to a function, and inject the result back into m
+  binj :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+  binj f a b = do { left <- a ; right <- b ; return (f left right) }
+
   {-- Grammar:
     Stmt := Term {('+'|'-') Stmt}
     Term := Factor {'*' Term}
-    Factor := Expr {('d'|'dc') Expr}      -- something like 4d6d20 is not allowed, use (4d6)d20
-    Expr := Number | 'p' | '(' Stmt ')'
+    Factor := Expr {('d'|'crit') Expr}      -- something like 4d6d20 is not allowed, use (4d6)d20
+    Expr := Number | 'perrin' | '(' Stmt ')'
   --}
 
-  type Tokens = [String]
+  -- | Parse the input, spit out an Expr
+  parser :: String -> ParseResult
+  parser s = lexer s >>= parseStmt 
 
-  -- Parse the input, spit out an Expr
-  parser :: String -> Expr
-  parser = parseStmt . lexer
+  -- | Custom lexSer, remove spaces and break the string apart into tokens. On Failure, return Left (errorMessage)
+  lexer :: String -> LexResult
+  lexer s = lexStr (filter (not . isSpace) s)
 
-  -- Custom lexer, remove spaces and break the string apart into tokens
-  lexer :: String -> Tokens
-  lexer [] = []
-  lexer s = breakItDown $ filter (not . isSpace) s
-    where breakItDown ('(':rest) = "LParen" : lexer rest
-          breakItDown (')':rest) = "RParen" : lexer rest
-          breakItDown ('c':'r':'i':'t':rest) = "Dc" : lexer rest
-          breakItDown ('c':rest) = "Dc" : lexer rest
-          breakItDown ('d':rest) = "D" : lexer rest
-          breakItDown ('p':'e':'r':'r':'i':'n':rest) = "P" : lexer rest                                   
-          breakItDown ('p':rest) = "P" : lexer rest
-          breakItDown ('*':rest) = "Mult" : lexer rest
-          breakItDown ('+':rest) = "Add" : lexer rest
-          breakItDown ('-':rest) = "Sub" : lexer rest
-          breakItDown (i:rest) | isDigit i = ("Value " ++ [i] ++ otherNumbers) : lexer remaining
-            where (otherNumbers, remaining) = span isDigit rest
-          breakItDown (e:errs) = error $ "Lexing error at " ++ [e] ++ ", rest of input is: " ++ errs
+  -- | Lex the string. If we encounter an error, just return an Error message, which is passed up the chain
+  lexStr :: [Char] -> ParseEither [[Char]]
+  lexStr ('(':rest)                     = lexStr rest >>= return . (:) "LParen"
+  lexStr (')':rest)                     = lexStr rest >>= return . (:) "RParen"
+  lexStr ('c':'r':'i':'t':rest)         = lexStr rest >>= return . (:) "Crit"
+  lexStr ('c':rest)                     = lexStr rest >>= return . (:) "Crit"
+  lexStr ('d':rest)                     = lexStr rest >>= return . (:) "D"
+  lexStr ('p':'e':'r':'r':'i':'n':rest) = lexStr rest >>= return . (:) "Perrin"
+  lexStr ('p':rest)                     = lexStr rest >>= return . (:) "Perrin"
+  lexStr ('*':rest)                     = lexStr rest >>= return . (:) "Mult"
+  lexStr ('+':rest)                     = lexStr rest >>= return . (:) "Add"
+  lexStr ('-':rest)                     = lexStr rest >>= return . (:) "Sub"
+  lexStr (i:more) | isDigit i           = lexStr rest >>= return . (:) ("Value " ++ [i] ++ otherNumbers)
+    where (otherNumbers, rest) = span isDigit more
+  lexStr (e:errs) = Error $ "Lexing error at " ++ [e] ++ ", rest of input is: " ++ errs
+  lexStr [] = return []
 
 
   -- The functions below parse their respective productions, as outlined in the grammar above
@@ -44,40 +65,41 @@ module Dnd.Dice.DiceParser where
   -- from anywhere, only from the top level, i.e. when they are not enclosed in any set of parenthesis
   -- The functions existsAtTop and topBreak handle this caveat for us.
 
-  parseStmt :: Tokens -> Expr
-  parseStmt ts | existsAtTop ["Add", "Sub"] ts = (constructor middle) (parseTerm left) (parseStmt right)
+  -- parseStmt :: Tokens -> ParseResult
+  parseStmt ts | existsAtTop ["Add", "Sub"] ts = binj (constructor middle) (parseTerm left) (parseStmt right)
     where (left,middle,right) = topBreak ["Add", "Sub"] ts
+          constructor :: String -> (Expr -> Expr -> Expr)
           constructor "Add" = Add
           constructor "Sub" = Sub
   parseStmt ts = parseTerm ts
 
-  parseTerm :: Tokens -> Expr
-  parseTerm ts | existsAtTop ["Mult"] ts = Mult (parseFactor left) (parseTerm right)
+  -- parseTerm :: Tokens -> ParseResult
+  parseTerm ts | existsAtTop ["Mult"] ts = binj Mult (parseFactor left) (parseTerm right)
     where (left, _, right) = topBreak ["Mult"] ts
   parseTerm ts = parseFactor ts
 
-  parseFactor :: Tokens -> Expr
-  parseFactor ts | existsAtTop ["D","Dc"] ts = (constructor middle) (parseExpr left) (parseExpr right)
-    where (left,middle,right) = topBreak ["D", "Dc"] ts
+  -- parseFactor :: Tokens -> Expr
+  parseFactor ts | existsAtTop ["D","Crit"] ts = binj (constructor middle) (parseExpr left) (parseExpr right)
+    where (left,middle,right) = topBreak ["D", "Crit"] ts
           constructor "D" = D
-          constructor "Dc" = Dc
+          constructor "Crit" = Crit
   parseFactor ts = parseExpr ts
 
-  parseExpr :: Tokens -> Expr
-  parseExpr [t] | startswith "Value " t = Value . read $ drop 6 t    -- a little hackish
-  parseExpr ["P"] = P
+  -- parseExpr :: Tokens -> Expr
+  parseExpr [t] | startswith "Value " t = return . Value . read $ drop 6 t    -- a little hackish
+  parseExpr ["Perrin"] = return Perrin
   parseExpr ("LParen":ts) = if last ts == "RParen" then parseStmt (dropLast ts)
-                            else error "Unbalanced Parenthesis"
+                            else Error "Parse error: Unbalanced Parenthesis"
     where dropLast xs = take (length xs - 1) xs
-  parseExpr xs = error $ concat xs
+  parseExpr xs = Error $ "Parse error: unknown parse error on " ++ concat xs
 
-  -- See if any of the given tokens is at the top level, i.e. not enclosed in parens
+  -- | See if any of the given tokens is at the top level, i.e. not enclosed in parens
   existsAtTop :: Tokens -> Tokens -> Bool
   existsAtTop ss ts = case topBreak ss ts of (_,"",_) -> False
                                              _ -> True
 
-  -- Break at the first occurance of an element of ss, when that element is currently on top.
-  -- Returns (prefix, element, suffix) 
+  -- | Break at the first occurance of an element of ss, when that element is currently on top.
+  -- Returns (prefix, element, suffix)
   -- Note that, unlike break, the element itself is the middle result instead of being on the suffix
   -- Assumes that ss doesn't contain a paren
   topBreak :: Tokens -> Tokens -> (Tokens, String, Tokens)
